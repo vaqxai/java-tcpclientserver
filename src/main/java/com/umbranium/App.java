41e852348b32d499befa55e64efe29c534ff4beb
Helios <vakkendwarf@gmail.com>
2021-11-14 17:28:12 +0100

    Initial commit

---
+++ src/main/java/com/umbranium/App.java
@@ -0,0 +1,13 @@
+package com.umbranium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
