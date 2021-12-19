dfd5702e5227b765d25dbc1fd7960a92e7322ed9
umbranium <vakkendwarf@gmail.com>
2021-12-20 00:48:29 +0100

    remove unused imports

--- src/main/java/io/github/vaqxai/UDPServerMulti.java
+++ src/main/java/io/github/vaqxai/UDPServerMulti.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.util.*;
-import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class UDPServerMulti extends UDPServer {
